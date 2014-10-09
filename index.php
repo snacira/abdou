0d1978aecd5dd90bae6886a7b34f598d6fbb88d8
marcelo <mclopds@gmail.com>
2014-10-09 15:19:39 +0200

    remplacer les table

--- index.php
+++ index.php
@@ -68,28 +68,64 @@
                 Je me charge de l'intendance de votre séjour : transport, hôtels et gites, repas. Je vous servirai de guide et d'interprète
                 et je vous trouverai les meilleurs endroits pour que vous puissiez assouvir votre passion.<br />
                 <span class="tagline">Vous ne faites que gonfler, voler, manger, rire, dormir. Le reste, je m'en occupe...</span><br /><br />
-                <table><tr>
-                    <td class="transport"></td>
-                    <td class="tddesc">
-                        <h3>Transport</h3>
-                        Transfert de et vers l'aéroport, navettes entre l'attéro et le déco, courses locales avant le départ...</td>
-                    <td class="hebergement"></td>
-                    <td class="tddesc">
-                        <h3>Hébergement</h3>
-                        Réservations d'hôtels et d'auberges confortables, directement sur le déco lorsque possible.
-                    </td>
-                </tr><tr>
-                    <td class="repas"></td>
-                    <td class="tddesc">
-                        <h3>Repas</h3>
-                        Réservation dans des restaurants typiques, préparation des déjeuners directement sur le site, au besoin.
-                    </td>
-                    <td class="guide"></td>
-                    <td class="tddesc">
-                        <h3>Guide</h3>
-                        Proposition de circuits en fonction de l'aérologie, visites locales, renseignements culturels.
-                    </td>
-                </tr></table>
+                <!--
+                <table>
+	                <tr>
+	                    <td class="transport"></td>
+	                    <td class="tddesc">
+	                        <h3>Transport</h3>
+	                        Transfert de et vers l'aéroport, navettes entre l'attéro et le déco, courses locales avant le départ...</td>
+	                    <td class="hebergement"></td>
+	                    <td class="tddesc">
+	                        <h3>Hébergement</h3>
+	                        Réservations d'hôtels et d'auberges confortables, directement sur le déco lorsque possible.
+	                    </td>
+	                </tr><tr>
+	                    <td class="repas"></td>
+	                    <td class="tddesc">
+	                        <h3>Repas</h3>
+	                        Réservation dans des restaurants typiques, préparation des déjeuners directement sur le site, au besoin.
+	                    </td>
+	                    <td class="guide"></td>
+	                    <td class="tddesc">
+	                        <h3>Guide</h3>
+	                        Proposition de circuits en fonction de l'aérologie, visites locales, renseignements culturels.
+	                    </td>
+	                </tr>
+	            </table>-->
+								<!--moncode-->
+
+				<div class="list">
+	                
+	                    <div class="transport"></div>
+	                    <div class="tddesc">
+	                        <h3>Transport</h3>
+	                        <p>Transfert de et vers l'aéroport, navettes entre l'attéro et le déco, courses locales avant le départ...</p>
+	                    </div>
+	                    
+	                    <div class="hebergement"></div>
+	                    <div class="tddesc">
+	                        <h3>Hébergement</h3>
+	                        <p>Réservations d'hôtels et d'auberges confortables, directement sur le déco lorsque possible.</p>
+	                    </div>
+	                
+
+	                    <div class="repas"></div>
+	                    <div class="tddesc">
+	                        <h3>Repas</h3>
+	                        <p>Réservation dans des restaurants typiques, préparation des déjeuners directement sur le site, au besoin.</p>
+	                    </div>
+	                    
+	                    <div class="guide"></div>
+	                    <div class="tddesc">
+	                        <h3>Guide</h3>
+	                        <p>Proposition de circuits en fonction de l'aérologie, visites locales, renseignements culturels.</p>
+	                    </div>
+	                
+	            </div>
+				
+
+
             </div>
             
             <br />
@@ -100,20 +136,52 @@
                 <!--Je m'adapte à chaque groupe et mes circuits sont flexibles. Cependant, ces 2 circuits sont plus souvent pratiqués, car ils offrent une grande variété de sites intéressants.<br><br>-->
                 
                2 circuits sont plus souvent pratiqués, car ils offrent une grande variété de sites intéressants. Cependant, je suis à votre disposition pour vous proposer d'autres destinations selon vos souhaits. Et, en cours de séjour, si la météo nous y oblige, nous pourrons bien sur modifier le parcours pour aller là où ça vole !<br /><br />
-                <table><tr>
-                    <td class="agadir"></td>
-                    <td class="tddesc">
-                        <h3>Agadir</h3>
-                        Transferts aéroport d'Agadir, vols littoral principalement, auberges pieds-dans-l'eau, jusqu'à 300m de dénivelé...
-                    <td class="marrakech"></td>
-                    <td class="tddesc">
-                        <h3>Marrakech</h3>
-                        Transferts aéroport de Marrakech, vols en montagne, vue sur les sommets de l'Atlas, longs vols thermiques...
-                    </td>
-                </tr></table>
+                
+			<!--		
+                <table>
+	                <tr>
+	                    <td class="agadir"></td>
+	                    <td class="tddesc">
+	                        <h3>Agadir</h3>
+	                        Transferts aéroport d'Agadir, vols littoral principalement, auberges pieds-dans-l'eau, jusqu'à 300m de dénivelé...
+	                    <td class="marrakech"></td>
+	                    <td class="tddesc">
+	                        <h3>Marrakech</h3>
+	                        Transferts aéroport de Marrakech, vols en montagne, vue sur les sommets de l'Atlas, longs vols thermiques...
+	                    </td>
+	                </tr>
+                </table>	-->
+
+							<!--mon code-->
+
+
+                <div class="list" id="circuit">
+	                
+	                
+	                    <div class="agadir"></div>
+	                    <div class="tddesc">
+	                        <h3>Agadir</h3>
+	                        <p>Transferts aéroport d'Agadir, vols littoral principalement, auberges pieds-dans-l'eau, jusqu'à 300m de dénivelé...</p>
+	                    </div>
+	                    
+
+
+	                    <div class="marrakech"></div>
+	                    <div class="tddesc">
+	                        <h3>Marrakech</h3>
+	                        <p>Transferts aéroport de Marrakech, vols en montagne, vue sur les sommets de l'Atlas, longs vols thermiques...</p>
+	                    </div>
+	                
+	                
+                </div> 
+
+
+
+
+
                 
                 <br />
-                <ul>
+                <ul class="clearboth">
                     <li>Disponible de septembre à janvier (de février à mai, contactez <a href="http://www.lesgensdair.com/maroc.htm" title="Voyage de parapente au Maroc" target="_blank">Les Gens d'Air</a>).</li>
                     <li>Pour voler avec moi, vous devez être un pilote breveté.</li>
                     <li>Avant le départ, vous devez impérativement avoir rempli les conditions d'autorisation de vol au Maroc.</li>
