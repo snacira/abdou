df28c08254db7b56aa02bc05b87320a775e71068
marcelo <mclopds@gmail.com>
2014-10-09 16:54:22 +0200

    contact php

---
+++ inc/top.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta name="keywords" content="guide parapente maroc">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Guide de parapente au Maroc | Abderahim Derbani</title>
+<meta name="Description" content="Abderrahim Derbani, guide de parapente au Maroc, sur les circuits d'Agadir et de Marrakech." />
+
+		
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+	
+	
+</head>
+
+<body>
+
+	<div id="container">
+    
+    <div id="header">
+        <div id="bijour">
+            <span class="font85">Bijour</span><span class="font29">. Je suis <strong>Abderrahim Derbani</strong></span><br/>
+            <span class="font23"> &nbsp;et je suis <strong><span class="font35">guide</span></strong> pour <strong><span class="font35">parapentistes</span></strong> au <strong><span class="font35">Maroc</span></strong></span>
+        </div>
+    </div>
