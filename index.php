db55849726572cf2c4b97d8192fddd5e4540dedd
nacira <s.nacira@gmail.com>
2014-10-09 14:53:26 +0200

    add files

--- index.php
+++ index.php
@@ -204,20 +204,10 @@
 <script type="text/javascript"  src="jquery-1.4.3.min.js"></script>
     
 <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+
 <script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
 <script type="text/javascript" src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script type="text/javascript">
-$("a[rel=shadowbox]").fancybox({
-				'cyclic' : 	true,
-				//'autoScale' : 	true,
-				'centerOnScroll ' :	true,
-				'overlayOpacity' : 	0.8,
-				'overlayColor' : '#000',
-				'titleShow' : 	false,
-				'transitionIn'	: 'fade',
-				'transitionOut'	: 'fade'
-			});
-</script>
+<script type="text/javascript" src="js/script.js"></script>
 
 	
     
---
+++ js/script.js
@@ -0,0 +1,10 @@
+$("a[rel=shadowbox]").fancybox({
+				'cyclic' : 	true,
+				//'autoScale' : 	true,
+				'centerOnScroll ' :	true,
+				'overlayOpacity' : 	0.8,
+				'overlayColor' : '#000',
+				'titleShow' : 	false,
+				'transitionIn'	: 'fade',
+				'transitionOut'	: 'fade'
+			});
